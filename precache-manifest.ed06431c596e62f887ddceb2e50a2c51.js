c5345dfab7ebaaff670ee1291202a9c2469c734e
Ilya Krasilnikov <kravus27@gmail.com>
2020-06-09 13:08:34 +0300

    Updates

---
+++ precache-manifest.ed06431c596e62f887ddceb2e50a2c51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14847b7b89ee1996705735bc14af91b",
+    "url": "/dashboard-project-new/index.html"
+  },
+  {
+    "revision": "428ead9bfa7fb2ae9b28",
+    "url": "/dashboard-project-new/static/css/main.2b24b216.chunk.css"
+  },
+  {
+    "revision": "01762ad4b68f6000297f",
+    "url": "/dashboard-project-new/static/js/2.87fb7e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard-project-new/static/js/2.87fb7e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428ead9bfa7fb2ae9b28",
+    "url": "/dashboard-project-new/static/js/main.4d8350b2.chunk.js"
+  },
+  {
+    "revision": "40a653cbea624fdd880a",
+    "url": "/dashboard-project-new/static/js/runtime-main.d6fefa92.js"
+  }
+]);
